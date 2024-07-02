4960c2bff0cb764912d6106ce5601d33c391ca85
Rumanda Senanayake <rumandasenanayake@gmail.com>
2024-07-02 21:33:41 +0530

    Chamu

--- food-del/frontend/src/App copy.jsx
+++ food-del/frontend/src/App copy.jsx
@@ -3,7 +3,10 @@ import Home from './pages/Home/Home'
 import Footer from './components/Footer/Footer'
 import Navbar from './components/Navbar/Navbar'
 import { Route, Routes } from 'react-router-dom'
-
+import Cart from './pages/Cart/Cart'
+import LoginPopup from './components/LoginPopup/LoginPopup'
+import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
+import MyOrders from './pages/MyOrders/MyOrders'
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Verify from './pages/Verify/Verify'
@@ -20,6 +23,7 @@ const App = () => {
         <Navbar setShowLogin={setShowLogin}/>
         <Routes>
           <Route path='/' element={<Home />}/>
+          <Route path='/cart' element={<Cart />}/>
           <Route path='/cart' element={<Cart />}/>
           <Route path='/order' element={<PlaceOrder />}/>
           <Route path='/myorders' element={<MyOrders />}/>
